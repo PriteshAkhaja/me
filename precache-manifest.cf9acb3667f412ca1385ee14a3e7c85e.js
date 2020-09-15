71999dadaa764e8938ab36673248311b44c48c15
ankit bavadiya <ankitbavadiya1011@gmail.com>
2020-09-15 15:31:36 +0530

    Updates

---
+++ precache-manifest.cf9acb3667f412ca1385ee14a3e7c85e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdad2128df2b6b5881a9",
+    "url": "/me/static/css/main.ecd764e8.chunk.css"
+  },
+  {
+    "revision": "bdad2128df2b6b5881a9",
+    "url": "/me/static/js/main.bdad2128.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b9136a632c48746cda9c519dce4f7a83",
+    "url": "/me/index.html"
+  }
+];
